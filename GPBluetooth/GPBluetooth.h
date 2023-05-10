c8643fd56f991a0a4918b9dc9aef17be5e63c745
hien bui <yuusaku.kd352911@gmail.com>
2023-05-10 20:19:36 +0700

    Initial Commit

---
+++ GPBluetooth/GPBluetooth.h
@@ -0,0 +1,19 @@
+//
+//  GPBluetooth.h
+//  GPBluetooth
+//
+//  Created by Duy Hien on 5/10/23.
+//  Copyright © 2023 Duy Hien. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GPBluetooth.
+FOUNDATION_EXPORT double GPBluetoothVersionNumber;
+
+//! Project version string for GPBluetooth.
+FOUNDATION_EXPORT const unsigned char GPBluetoothVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GPBluetooth/PublicHeader.h>
+
+
