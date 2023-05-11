cf707e9ce0164bb29b9076d2e3a5e954c7ea6b1a
bui.duy.hien <bui.duy.hien@play.jp>
2023-05-11 08:16:17 +0700

    add Student file

---
+++ GPBluetooth/Student.swift
@@ -0,0 +1,13 @@
+//
+//  Student.swift
+//  GPBluetooth
+//
+//  Created by Duy Hien on 11/05/2023.
+//  Copyright © 2023 Duy Hien. All rights reserved.
+//
+
+import Foundation
+
+public struct Student {
+    public let name: String
+}
